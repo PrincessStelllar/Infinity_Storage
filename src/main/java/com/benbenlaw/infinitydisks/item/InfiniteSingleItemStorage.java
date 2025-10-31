0f6f2e8361d87b6c37911351b61d1fb0fe02036d
benbenlaw <benlawrenceben@hotmail.co.uk>
2025-10-31 19:17:28 +0000

    1.0.0

---
+++ src/main/java/com/benbenlaw/infinitydisks/item/InfiniteSingleItemStorage.java
@@ -0,0 +1,60 @@
+package com.benbenlaw.infinitydisks.item;
+
+import com.refinedmods.refinedstorage.api.core.Action;
+import com.refinedmods.refinedstorage.api.resource.ResourceAmount;
+import com.refinedmods.refinedstorage.api.resource.ResourceKey;
+import com.refinedmods.refinedstorage.api.storage.Actor;
+import com.refinedmods.refinedstorage.api.storage.Storage;
+import com.refinedmods.refinedstorage.common.api.storage.SerializableStorage;
+import com.refinedmods.refinedstorage.common.api.storage.StorageType;
+import com.refinedmods.refinedstorage.common.storage.StorageTypes;
+import com.refinedmods.refinedstorage.common.support.resource.ItemResource;
+import net.minecraft.nbt.CompoundTag;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.List;
+
+public class InfiniteSingleItemStorage implements SerializableStorage {
+
+    private final ItemStack infinityStack;
+
+    public InfiniteSingleItemStorage(ItemStack itemStack) {
+        this.infinityStack = itemStack.copy();
+    }
+
+    @Override
+    public long insert(ResourceKey resource, long amount, Action action, Actor actor) {
+        if (resource instanceof ItemResource item &&
+                ItemStack.isSameItemSameComponents(item.toItemStack(), infinityStack)) {
+            return amount;
+        }
+        return 0;
+    }
+
+    @Override
+    public long extract(ResourceKey resource, long amount, Action action, Actor actor) {
+        if (resource instanceof ItemResource item &&
+                ItemStack.isSameItemSameComponents(item.toItemStack(), infinityStack)) {
+            return amount;
+        }
+        return 0;
+    }
+
+    @Override
+    public Collection<ResourceAmount> getAll() {
+        return List.of(new ResourceAmount(ItemResource.ofItemStack(infinityStack), Long.MAX_VALUE));
+    }
+
+    @Override
+    public long getStored() {
+        return Long.MAX_VALUE;
+    }
+
+    @Override
+    public StorageType getType() {
+        return StorageTypes.ITEM;
+    }
+}
---
+++ src/main/java/com/benbenlaw/infinitydisks/item/InfinityDiskItem.java
@@ -0,0 +1,90 @@
+package com.benbenlaw.infinitydisks.item;
+
+import com.refinedmods.refinedstorage.api.core.Action;
+import com.refinedmods.refinedstorage.api.resource.ResourceAmount;
+import com.refinedmods.refinedstorage.api.resource.ResourceKey;
+import com.refinedmods.refinedstorage.api.storage.Actor;
+import com.refinedmods.refinedstorage.common.storage.ItemStorageVariant;
+import com.refinedmods.refinedstorage.common.storage.StorageTypes;
+import com.refinedmods.refinedstorage.common.storage.StorageVariant;
+import com.refinedmods.refinedstorage.common.storage.storagedisk.ItemStorageDiskItem;
+import com.refinedmods.refinedstorage.common.api.storage.SerializableStorage;
+import com.refinedmods.refinedstorage.common.api.storage.StorageRepository;
+import com.refinedmods.refinedstorage.common.support.resource.ItemResource;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.item.ItemStack;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+public class InfinityDiskItem extends ItemStorageDiskItem {
+
+    private final ItemStack infinityStack;
+    private final Component helpText;
+
+    public InfinityDiskItem(ItemStack itemStack) {
+        super(ItemStorageVariant.CREATIVE);
+        this.infinityStack = itemStack.copy();
+        this.helpText = Component.literal("UNLIMITED STORAGE");
+    }
+
+    @Override
+    public StorageVariant getVariant() {
+        return ItemStorageVariant.CREATIVE;
+    }
+
+    /**
+     * Delegate to the official Creative Storage disk backend,
+     * then wrap it to only expose your single item.
+     */
+    @Override
+    protected SerializableStorage createStorage(StorageRepository storageRepository) {
+        // Official creative storage disk backend
+        SerializableStorage base = StorageTypes.ITEM.create(null, storageRepository::markAsChanged);
+
+        // Wrap it so that only our single item appears in getAll()
+        return new SerializableStorage() {
+            @Override
+            public long insert(ResourceKey resourceKey, long amount, Action action, Actor actor) {
+                if (resourceKey instanceof ItemResource item &&
+                        ItemStack.isSameItemSameComponents(item.toItemStack(), infinityStack)) {
+                    return amount; // allow full insert
+                }
+                return 0; // reject everything else
+            }
+
+            @Override
+            public long extract(ResourceKey resourceKey, long amount, Action action, Actor actor) {
+                if (resourceKey instanceof ItemResource item &&
+                        ItemStack.isSameItemSameComponents(item.toItemStack(), infinityStack)) {
+                    return amount; // allow full extract
+                }
+                return 0;
+            }
+
+            @Override
+            public Collection<ResourceAmount> getAll() {
+                // Return exactly one ResourceAmount: our infinityStack, with infinite count
+                return List.of(new ResourceAmount(ItemResource.ofItemStack(infinityStack), Long.MAX_VALUE));
+            }
+
+
+            @Override
+            public long getStored() {
+                return Long.MAX_VALUE; // always infinite
+            }
+
+            @Override
+            public com.refinedmods.refinedstorage.common.api.storage.StorageType getType() {
+                return base.getType();
+            }
+        };
+    }
+
+    public ItemStack getInfinityStack() {
+        return infinityStack.copy();
+    }
+}
